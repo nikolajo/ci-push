69069d740dd2a142f707646dbb6c274351e7771c
NPO <NPO@topdanmark.dk>
2014-06-03 15:38:52 +0200

    Added Apache License

--- Server/ESB/push-trigger-flow/source/push-trigger/src/main/java/org/pushtrigger/mule/agent/CreateQueueAgent.java
+++ Server/ESB/push-trigger-flow/source/push-trigger/src/main/java/org/pushtrigger/mule/agent/CreateQueueAgent.java
@@ -14,6 +14,20 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 /**
+ * Copyright 2014 Nikolaj Ougaard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+
  * This agent class runs at startup and creates the queue required by the push server amqp interface
  * 
  * @author Nikolaj Ougaard
